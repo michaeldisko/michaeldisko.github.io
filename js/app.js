cb309f288a63b2b39767dc7c1f689cca4392dc64
mikedisko <disko.frontend@gmail.com>
2016-07-20 12:47:42 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,8 @@
+console.log('its ok');
+$('#fullpage').fullpage({
+	verticalCentered: false,
+	anchors: ['firstPage', 'secondPage'],
+    menu: '#menu',
+    css3: true,
+    scrollingSpeed: 1000
+});
---
+++ readme.md
@@ -0,0 +1,1 @@
+# mikedisko.github.io
